5bd869add02d84b4d43750f7d006878673bbb469
cviesca <carmen.viesca@wheregroup.com>
2024-04-04 14:44:30 +0200

    update and use PLUGIN_SETTINGS_SERVER_CONFIG_KEY, delete prints

--- mapbender_plugin/server_config.py
+++ mapbender_plugin/server_config.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 
 from qgis._core import QgsSettings, QgsApplication, QgsAuthMethodConfig
 
+from mapbender_plugin.settings import PLUGIN_SETTINGS_SERVER_CONFIG_KEY
+
 
 @dataclass
 class ServerConfig:
@@ -18,25 +20,25 @@ class ServerConfig:
     # @staticmethod
     # def saveToSettings(name, url, port, username, password, projects_path, mb_app_path, mb_basis_url):
     #     s = QgsSettings()
-    #     s.setValue(f"mapbender-plugin/connection/{name}/url", url)
-    #     s.setValue(f"mapbender-plugin/connection/{name}/port", port)
-    #     s.setValue(f"mapbender-plugin/connection/{name}/username", username)
-    #     s.setValue(f"mapbender-plugin/connection/{name}/password", password)
-    #     s.setValue(f"mapbender-plugin/connection/{name}/projects_path", projects_path)
-    #     s.setValue(f"mapbender-plugin/connection/{name}/mb_app_path", mb_app_path)
-    #     s.setValue(f"mapbender-plugin/connection/{name}/mb_basis_url", mb_basis_url)
+    #     s.setValue(f"{PLUGIN_SETTINGS_SERVER_CONFIG_KEY}/connection/{name}/url", url)
+    #     s.setValue(f"{PLUGIN_SETTINGS_SERVER_CONFIG_KEY}/connection/{name}/port", port)
+    #     s.setValue(f"{PLUGIN_SETTINGS_SERVER_CONFIG_KEY}/connection/{name}/username", username)
+    #     s.setValue(f"{PLUGIN_SETTINGS_SERVER_CONFIG_KEY}/connection/{name}/password", password)
+    #     s.setValue(f"{PLUGIN_SETTINGS_SERVER_CONFIG_KEY}/connection/{name}/projects_path", projects_path)
+    #     s.setValue(f"{PLUGIN_SETTINGS_SERVER_CONFIG_KEY}/connection/{name}/mb_app_path", mb_app_path)
+    #     s.setValue(f"{PLUGIN_SETTINGS_SERVER_CONFIG_KEY}/connection/{name}/mb_basis_url", mb_basis_url)
 
     def save(self):
         s = QgsSettings()
-        s.setValue(f"mapbender-plugin/connection/{self.name}/url", self.url)
-        s.setValue(f"mapbender-plugin/connection/{self.name}/port", self.port)
-        # s.setValue(f"mapbender-plugin/connection/{self.name}/username", self.username)
-        # s.setValue(f"mapbender-plugin/connection/{self.name}/password", self.password)
-        s.setValue(f"mapbender-plugin/connection/{self.name}/username", '')
-        s.setValue(f"mapbender-plugin/connection/{self.name}/password", '')
-        s.setValue(f"mapbender-plugin/connection/{self.name}/projects_path", self.projects_path)
-        s.setValue(f"mapbender-plugin/connection/{self.name}/mb_app_path", self.mb_app_path)
-        s.setValue(f"mapbender-plugin/connection/{self.name}/mb_basis_url", self.mb_basis_url)
+        s.setValue(f"{PLUGIN_SETTINGS_SERVER_CONFIG_KEY}/connection/{self.name}/url", self.url)
+        s.setValue(f"{PLUGIN_SETTINGS_SERVER_CONFIG_KEY}/connection/{self.name}/port", self.port)
+        # s.setValue(f"{PLUGIN_SETTINGS_SERVER_CONFIG_KEY}/connection/{self.name}/username", self.username)
+        # s.setValue(f"{PLUGIN_SETTINGS_SERVER_CONFIG_KEY}/connection/{self.name}/password", self.password)
+        s.setValue(f"{PLUGIN_SETTINGS_SERVER_CONFIG_KEY}/connection/{self.name}/username", '')
+        s.setValue(f"{PLUGIN_SETTINGS_SERVER_CONFIG_KEY}/connection/{self.name}/password", '')
+        s.setValue(f"{PLUGIN_SETTINGS_SERVER_CONFIG_KEY}/connection/{self.name}/projects_path", self.projects_path)
+        s.setValue(f"{PLUGIN_SETTINGS_SERVER_CONFIG_KEY}/connection/{self.name}/mb_app_path", self.mb_app_path)
+        s.setValue(f"{PLUGIN_SETTINGS_SERVER_CONFIG_KEY}/connection/{self.name}/mb_basis_url", self.mb_basis_url)
 
         authCfgId = ServerConfig.save_basic_to_auth_db(self.name, self.username, self.password)
         s.setValue(f"mapbender-plugin/connection/{self.name}/authcfg", authCfgId)
@@ -54,7 +56,6 @@ class ServerConfig:
         auth_manager = QgsApplication.authManager()
         # set Basic
         conf = QgsAuthMethodConfig()
-        print(conf)
         conf.setName(server_name)
         conf.setMethod("Basic")
         conf.setConfig("username", username)
@@ -67,20 +68,19 @@ class ServerConfig:
         auth_manager.storeAuthenticationConfig(conf)
         newAuthCfgId = conf.id()
         assert newAuthCfgId
-        print(newAuthCfgId)
         return newAuthCfgId
 
     @staticmethod
     def getParamsFromSettings(name: str):
         s = QgsSettings()
-        url = s.value(f"mapbender-plugin/connection/{name}/url")
-        port = s.value(f"mapbender-plugin/connection/{name}/port")
-        #username = s.value(f"mapbender-plugin/connection/{name}/username")
-        #password = s.value(f"mapbender-plugin/connection/{name}/password")
-        projects_path = s.value(f"mapbender-plugin/connection/{name}/projects_path")
-        mb_app_path = s.value(f"mapbender-plugin/connection/{name}/mb_app_path")
-        mb_basis_url = s.value(f"mapbender-plugin/connection/{name}/mb_basis_url")
-        authcfg = s.value(f"mapbender-plugin/connection/{name}/authcfg")
+        url = s.value(f"{PLUGIN_SETTINGS_SERVER_CONFIG_KEY}/connection/{name}/url")
+        port = s.value(f"{PLUGIN_SETTINGS_SERVER_CONFIG_KEY}/connection/{name}/port")
+        #username = s.value(f"{PLUGIN_SETTINGS_SERVER_CONFIG_KEY}/connection/{name}/username")
+        #password = s.value(f"{PLUGIN_SETTINGS_SERVER_CONFIG_KEY}/connection/{name}/password")
+        projects_path = s.value(f"{PLUGIN_SETTINGS_SERVER_CONFIG_KEY}/connection/{name}/projects_path")
+        mb_app_path = s.value(f"{PLUGIN_SETTINGS_SERVER_CONFIG_KEY}/connection/{name}/mb_app_path")
+        mb_basis_url = s.value(f"{PLUGIN_SETTINGS_SERVER_CONFIG_KEY}/connection/{name}/mb_basis_url")
+        authcfg = s.value(f"{PLUGIN_SETTINGS_SERVER_CONFIG_KEY}/connection/{name}/authcfg")
 
         username, password = ServerConfig.get_username_and_password_from_auth_db(authcfg)
 
