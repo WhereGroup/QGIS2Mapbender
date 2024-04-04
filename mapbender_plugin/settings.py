30682dc2fafb2cf41517bdbd9cafabb1c7d3a379
cviesca <carmen.viesca@wheregroup.com>
2024-04-04 15:04:12 +0200

    delete SERVER_MB_CD_APPLICATION_PATH

--- mapbender_plugin/settings.py
+++ mapbender_plugin/settings.py
@@ -3,4 +3,3 @@
 # General
 PLUGIN_SETTINGS_SERVER_CONFIG_KEY = 'mapbender-plugin'
 SERVER_TABLE_HEADERS = ["Name", "URL", "QGIS-Projects path", "Mapbender app path", "Mapbender basis URL"]
-SERVER_MB_CD_APPLICATION_PATH = 'cd ..; cd /data/mapbender/application/;'
