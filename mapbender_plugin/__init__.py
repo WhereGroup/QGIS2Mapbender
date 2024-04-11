314dc7f7693a0b109a83036db995e8e5cc0198c0
pgipper <peter.gipper@wheregroup.com>
2024-04-11 18:48:48 +0200

    add license info text box

--- mapbender_plugin/__init__.py
+++ mapbender_plugin/__init__.py
@@ -1,3 +1,26 @@
+"""
+/***************************************************************************
+ Mapbender Plugin
+ A plugin to publish QGIS Projects via a Mapbender server instance.
+                             -------------------
+        begin                : 2024-02-12
+        copyright            : (C) 2024 by WhereGroup GmbH
+        email                : carmen.viesca@wheregroup.com
+        git sha              : $Format:%H$
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ This script initializes the plugin, making it known to QGIS.
+"""
+
+
 def classFactory(iface):
     from .plugin import Mapbenderplugin
     return Mapbenderplugin(iface)
