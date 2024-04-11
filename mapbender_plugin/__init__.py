72a8c17defdc8eae2d9dc6ef8274ad1c76babf02
pgipper <peter.gipper@wheregroup.com>
2024-04-11 18:47:26 +0200

    reduce log noise

--- mapbender_plugin/__init__.py
+++ mapbender_plugin/__init__.py
@@ -1,6 +1,4 @@
-from qgis.core import QgsMessageLog
 def classFactory(iface):
     from .plugin import Mapbenderplugin
-    QgsMessageLog.logMessage('iface', 'iface')
     return Mapbenderplugin(iface)
 
